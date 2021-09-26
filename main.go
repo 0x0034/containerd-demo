414bce038ef0ca996f72ce1d541346b3dcfcdabe
ruochen <ruochen@mogujie.com>
2021-09-26 17:26:59 +0800

    first commit

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
+import (
+	"log"
 
+	"github.com/containerd/containerd"
+)
 
-func main(){
+func main() {
+	if err := redisExample(); err != nil {
+		log.Fatal(err)
+	}
+}
 
+func redisExample() error {
+	client, err := containerd.New("/run/containerd/containerd.sock")
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+	return nil
 }
